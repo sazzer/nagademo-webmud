07aeaf9c07114264b19c8ae4f6c83c6f54dd0ee4
Graham Cox <graham.cox@oclc.org>
2012-06-01 16:36:19 +0100

    Working integration of LESS for Stylesheets, and working index page with all dependencies existing

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-var express = require('express'),
+var path = require("path"),
+    express = require('express'),
     winston = require("winston"),
     yui3 = require("express-yui3"),
     app = express.createServer(),
-    io = require("socket.io").listen(app);
+    io = require("socket.io").listen(app),
+    
+    public = path.join(__dirname, "webapp/public");
+
+require("less");
 
 app.configure(function() {
     app.use(express.bodyParser()); // Allows us to parse HTTP Request bodies into JSON Objects
     app.use(express.cookieParser()); // Allows us to understand cookies
     app.use(express.session({secret: "0785ea65-ece7-4735-bf8c-1c131d78aa6c"})); // Allows us to use sessions
-    app.use(express.compiler({ src: __dirname + '/webapp/public', enable: ['less']})); // Compile Less stylesheets into CSS ones
-    app.use(express.static(__dirname + '/webapp/public')); // Make everything in /webapp/public available
-    app.use("/yui3", yui3.handler(__dirname + "/webapp/yui3"));
+    app.use(express.compiler({ src: public, enable: ['less']})); // Compile Less stylesheets into CSS ones
+    app.use(express.static(public)); // Make everything in /webapp/public available
+    app.use("/yui3", yui3.handler(path.join(__dirname, "webapp/yui3")));
     app.use(app.router);
 });
 
