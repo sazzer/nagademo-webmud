78647e8477261210ca989d890142cc3d0c0e0320
Graham Cox <graham.cox@oclc.org>
2012-06-08 13:08:27 +0100

    Trashed the Statistics class in favour of the new idea

--- webapp/yui3/characters/character.js
+++ webapp/yui3/characters/character.js
@@ -7,21 +7,6 @@ YUI.add("webmud-character", function(Y) {
         Y.Model,
         [],
         {
-            _setStats: function(v) {
-                if (v instanceof (Y.WebMud.Character.Stats)) {
-                    return v;
-                }
-                else {
-                    var stats = this.get("stats");
-                    if (!stats) {
-                        stats = new Y.WebMud.Character.Stats(v);
-                    }
-                    else {
-                        stats.setAttrs(v);
-                    }
-                    return stats;
-                }
-            }
         }, {
             ATTRS: {
                 /** The ID of the character */
@@ -32,11 +17,6 @@ YUI.add("webmud-character", function(Y) {
                 name: {},
                 /** The gender of the character */
                 gender: {},
-                /** The statistics of the character */
-                stats: {
-                    value: new Y.WebMud.Character.Stats(),
-                    setter: "_setStats"
-                },
                 /** When the character was created */
                 created: {
                     writeOnce: true
